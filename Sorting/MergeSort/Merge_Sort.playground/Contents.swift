42cafe19471c8a88922fcb31e80a3422c7ca5a34
MD AL MAMUN <shohag.csesust@gmail.com>
2018-11-11 02:15:43 -0500

    merge sort

---
+++ Sorting/MergeSort/Merge_Sort.playground/Contents.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+func merge(_ array: inout[Int], low: Int, mid: Int, high: Int) {
+    let aux = Array(array)
+    var i = low
+    var j = mid + 1
+    var k = low
+    while k <= high {
+        // If lower part exhausted then keep copying
+        // upper part rest of the elements
+        if i > mid {
+            array[k] = aux[j]
+            j += 1
+        }
+        // if upper part exhausted then keep copying
+        // lower part rest of the elements
+        else if j > high {
+            array[k] = aux[i]
+            i += 1
+        } else if aux[i] < aux[j] {
+            array[k] = aux[i]
+            i += 1
+        } else {
+            array[k] = aux[j]
+            j += 1
+        }
+        k += 1
+    }
+}
+
+func mergeSort(_ array: inout[Int], low: Int, high: Int) {
+    if low < high {
+        let mid = low + (high - low) / 2
+        mergeSort(&array, low: low, high: mid)
+        mergeSort(&array, low: mid + 1, high: high)
+        merge(&array, low: low, mid: mid, high: high)
+    }
+}
+
+var array = [3, 2, 1, 4, 5, 6, 7]
+mergeSort(&array, low: 0, high: array.count - 1)
+print(array)
