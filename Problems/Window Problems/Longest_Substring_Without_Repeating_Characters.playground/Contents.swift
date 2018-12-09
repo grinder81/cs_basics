d2b221828ac93cbe4fda07788814c3c2b1df3160
MD AL MAMUN <shohag.csesust@gmail.com>
2018-12-09 14:13:22 -0500

    sliding window problems

---
+++ Problems/Window Problems/Longest_Substring_Without_Repeating_Characters.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+func lengthOfLongestSubstring(_ s: String) -> Int {
+    if s.count == 0 { return 0 }
+    var l = 0
+    var r = 1
+    let n = s.count
+    let array = Array(s)
+    
+    var maxLen = -1
+    var hash: Dictionary<Character, Int> = [:]
+    
+    hash[array[0]] = 0
+    while r < n {
+        if let index = hash[array[r]], index >= l {
+            if (r - l) > maxLen {
+                maxLen = r - l
+            }
+            l = index + 1
+        }
+        hash[array[r]] = r
+        r += 1
+    }
+    if (r - l) > maxLen {
+        maxLen = r - l
+    }
+    return maxLen
+}
+
+lengthOfLongestSubstring("abc")
+lengthOfLongestSubstring("aaaa")
+lengthOfLongestSubstring("dvdf")
+lengthOfLongestSubstring("aaaaaaaabc")
+lengthOfLongestSubstring("a")
